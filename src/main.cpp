996b9ca1b9dcb36325d80255ec0287bc021ce7d4
Bruno Dias <contact@diasbruno.com.br>
2013-01-24 23:17:27 -0300

    update main.

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,19 @@
 #include "ofAppGlutWindow.h"
 
 #include "cpptest.h"
-#include "test_ofVec3f.h"
+#include "ofVec3f_test.h"
 
 void run_tests()
 {
     Test::Suite ts;
-    ts.add(auto_ptr<Test::Suite>(new ofVec3f_Suite));
+    ts.add( auto_ptr<Test::Suite>( new ofVec3f_Suite ) );
     
-    Test::TextOutput output(Test::TextOutput::Verbose);
-    ts.run(output);
+    Test::TextOutput output( Test::TextOutput::Verbose );
+    ts.run( output );
 }
 
 //========================================================================
-int main( ){
+int main() {
 	run_tests();
     return 0;
 }
