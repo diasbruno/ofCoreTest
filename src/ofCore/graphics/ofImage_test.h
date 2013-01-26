33dcb084b7c7bc653275f496003b5f59f75b8def
Bruno Dias <contact@diasbruno.com.br>
2013-01-26 14:02:34 -0300

    added ofImage tests.

---
+++ src/ofCore/graphics/ofImage_test.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "cpptest.h"
+#include "ofImage.h"
+
+using namespace std;
+
+class ofImage_Suite : public Test::Suite {
+public:
+	
+    ofImage_Suite() 
+    { 
+        TEST_ADD( ofImage_Suite::test_instance_creation     )
+//		ofPixels pixels = tImage.getPixels();
+//		if ( pixels.isAllocated() ) {
+//			TEST_ADD( ofImage_Suite::test_initialized_pixels    )
+//		}
+    }
+    
+    ofImage tImage;
+	
+protected:
+	
+    virtual void setup()     {}
+    virtual void tear_down() {}
+	
+private:
+	
+    void test_instance_creation() {
+		TEST_ASSERT( tImage.width == 0 )
+		TEST_ASSERT( tImage.height == 0 )
+		TEST_ASSERT( tImage.bpp == 0 )
+		TEST_ASSERT( tImage.type == OF_IMAGE_UNDEFINED )
+		TEST_ASSERT( tImage.isUsingTexture() == true )
+	}
+	
+	void test_initialized_pixels() {
+		// the default initialization of ofImage
+		// don't allocate any pixels on the initialization.
+		// this should be the correct behavior?
+//		TEST_ASSERT( tImage.getPixels().isAllocated() == false )
+//		TEST_ASSERT( tImage.getPixels().getWidth() == 0 )
+//		TEST_ASSERT( tImage.getPixels().getHeight() == 0 )
+	}
+};
