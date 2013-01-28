760fb29ee954139b07ddf69cf36e9185a067e60d
Bruno Dias <contact@diasbruno.com.br>
2013-01-28 04:48:10 -0300

    added tests.

--- src/ofCore/utils/ofUtils_test.h
+++ src/ofCore/utils/ofUtils_test.h
@@ -12,26 +12,40 @@ class ofUtils_DateTime_Suite
 public:
 	
     ofUtils_DateTime_Suite() { 
-        TEST_ADD( ofUtils_DateTime_Suite::test_get_hours )
+        TEST_ADD( ofUtils_DateTime_Suite::test_get_hour    )
+		TEST_ADD( ofUtils_DateTime_Suite::test_get_minutes )
+		TEST_ADD( ofUtils_DateTime_Suite::test_get_seconds )
     }
 	
+	time_t current;
 	tm local;
 		
 protected:
 	
-    void     setup() {
-		time_t 	curr;
-		time(&curr);
-		local = *(localtime(&curr));
-	}
-    void tear_down() {
-
-	}
+    void     setup() {}
+    void tear_down() {}
 	
 private:
 	
-    void test_get_hours() {
+	/*!
+	 NOTE: must update the time before each test.
+	 */
+	void updateTime() {
+		time(&current);
+		local = *(localtime(&current));		
+	}	
+	
+    void test_get_hour() {
+		updateTime();
 		TEST_ASSERT( ofGetHours() == local.tm_hour )
+	}
+	void test_get_minutes() {	
+		updateTime();
+		TEST_ASSERT( ofGetMinutes() == local.tm_min )
+	}
+	void test_get_seconds() {	
+		updateTime();
+		TEST_ASSERT( ofGetSeconds() == local.tm_sec )
 	}
 };
 
