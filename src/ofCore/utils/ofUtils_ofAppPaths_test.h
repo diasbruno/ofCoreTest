c559e4f8d67c1b7cca46dc40e731096f4575909f
Bruno Dias <contact@diasbruno.com.br>
2013-02-01 18:28:10 -0300

    adde tests for paths.

---
+++ src/ofCore/utils/ofUtils_ofAppPaths_test.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "cpptest.h"
+
+#include <limits>
+#include "ofMain.h"
+
+using namespace std;
+
+class ofUtils_ofAppPaths_Suite
+	: public Test::Suite {
+public:
+	
+    ofUtils_ofAppPaths_Suite() { 
+       // TEST_ADD( ofUtils_ofAppPaths_Suite:: )
+    }
+	
+protected:
+	
+    void     setup() {}
+    void tear_down() {}
+	
+private:
+	
+};
