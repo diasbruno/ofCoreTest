0b19d1a879ef37612c5f0b397e8de33acd3a3f04
Bruno Dias <contact@diasbruno.com.br>
2013-02-02 15:06:17 -0300

    added ofVecs operations test suite and more tests.

---
+++ src/ofCore/math/ofVecT_Operations_test.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "cpptest.h"
+#include "ofVec.h"
+
+using namespace std;
+
+class ofVecT_Operations_Suite : public Test::Suite {
+public:
+	
+    ofVecT_Operations_Suite() { 
+		TEST_ADD( ofVecT_Operations_Suite::test_operations_2d )
+		TEST_ADD( ofVecT_Operations_Suite::test_operations_3d )
+		TEST_ADD( ofVecT_Operations_Suite::test_operation_with_different_types )
+	}
+	
+protected:
+	
+	of::ofVec2f v2f;
+	of::ofVec3f v3f;
+	of::ofVec4f v4f;
+	
+    void     setup() {}
+    void tear_down() {}
+	
+private:
+
+    void test_operations_2d() {
+        
+        v2f.set( 10, 10 );
+		TEST_ASSERT( v2f.x == 10 )
+        TEST_ASSERT( v2f.y == 10 )
+
+        v2f += 10;
+		TEST_ASSERT( v2f.x == 20 )
+        TEST_ASSERT( v2f.y == 20 )
+    
+        v2f -= 10;
+		TEST_ASSERT( v2f.x == 10 )
+        TEST_ASSERT( v2f.y == 10 )
+
+        v2f *= 10;
+		TEST_ASSERT( v2f.x == 100 )
+        TEST_ASSERT( v2f.y == 100 )
+
+        v2f /= 10;
+		TEST_ASSERT( v2f.x == 10 )
+        TEST_ASSERT( v2f.y == 10 )
+	}
+	
+	void test_operations_3d() {
+        
+        v3f.set( 10, 10, 10 );
+		TEST_ASSERT( v3f.x == 10 )
+        TEST_ASSERT( v3f.y == 10 )
+		TEST_ASSERT( v3f.y == 10 )
+		
+        v3f += 10;
+		TEST_ASSERT( v3f.x == 20 )
+        TEST_ASSERT( v3f.y == 20 )
+		TEST_ASSERT( v3f.z == 20 )
+		
+        v3f -= 10;
+		TEST_ASSERT( v3f.x == 10 )
+        TEST_ASSERT( v3f.y == 10 )
+		TEST_ASSERT( v3f.z == 10 )
+		
+        v3f *= 10;
+		TEST_ASSERT( v3f.x == 100 )
+        TEST_ASSERT( v3f.y == 100 )
+		TEST_ASSERT( v3f.z == 100 )
+		
+        v3f /= 10;
+		TEST_ASSERT( v3f.x == 10 )
+        TEST_ASSERT( v3f.y == 10 )
+		TEST_ASSERT( v3f.z == 10 )
+	}
+	
+	void test_operation_with_different_types() {
+		
+		of::ofVec2<int> v2i(30, 40);
+		v2f += v2i;
+		TEST_ASSERT( v2f.x == 40 )
+        TEST_ASSERT( v2f.y == 50 )
+		
+		v2f += (int)40;
+		TEST_ASSERT( v2f.x == 80 )
+        TEST_ASSERT( v2f.y == 90 )
+    }
+
+};
--- src/ofCore/math/ofVecT_test.h
+++ src/ofCore/math/ofVecT_test.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "cpptest.h"
-#include "ofVecT.h"
+#include "ofVec.h"
 
 using namespace std;
 
@@ -12,6 +12,8 @@ public:
         TEST_ADD( ofVecT_Suite::test_instance_creation_2d )
 		TEST_ADD( ofVecT_Suite::test_instance_creation_3d )
 		TEST_ADD( ofVecT_Suite::test_instance_creation_4d )
+		TEST_ADD( ofVecT_Suite::test_equality )
+		TEST_ADD( ofVecT_Suite::test_assignments )
 	}
 	
 protected:
@@ -23,44 +25,92 @@ private:
 	
     void test_instance_creation_2d() {
 		
-		of::ofVec2f vec2f( 10.0f, 10.0f );
-		of::ofVec2d vec2d( 20.0, 20.0 );
+		of::ofVec2f v2f( 10.0f, 10.0f );
+		TEST_ASSERT( v2f.x == 10.0f )
+		TEST_ASSERT( v2f.y == 10.0f )
+
+        of::ofVec2f nv2f;
+		nv2f.set( v2f );
+		TEST_ASSERT( nv2f.x == 10.0f )
+		TEST_ASSERT( nv2f.y == 10.0f )
 		
-		TEST_ASSERT( vec2f.x == 10.0f )
-		TEST_ASSERT( vec2f.y == 10.0f )
-		
-		TEST_ASSERT( vec2d.x == 20.0 )
-		TEST_ASSERT( vec2d.y == 20.0 )
+		of::ofVec2<int> v2i( 10, 40 );
+		TEST_ASSERT( v2i.x == 10 )
+		TEST_ASSERT( v2i.y == 40 )
 	}
 	
 	void test_instance_creation_3d() {
 		
-		of::ofVec3f vec3f( 10.0f, 10.0f, 50.0f );
-		of::ofVec3d vec3d( 20.0, 20.0, 50.0 );
+		of::ofVec3f v3f( 10.0f, 10.0f, 50.0f );
+		TEST_ASSERT( v3f.x == 10.0f )
+		TEST_ASSERT( v3f.y == 10.0f )
+		TEST_ASSERT( v3f.z == 50.0f )
 		
-		TEST_ASSERT( vec3f.x == 10.0f )
-		TEST_ASSERT( vec3f.y == 10.0f )
-		TEST_ASSERT( vec3f.z == 50.0f )
-		
-		TEST_ASSERT( vec3d.x == 20.0 )
-		TEST_ASSERT( vec3d.y == 20.0 )
-		TEST_ASSERT( vec3d.z == 50.0 )
+		of::ofVec3d v3d( 20.0, 20.0, 50.0 );
+		TEST_ASSERT( v3d.x == 20.0 )
+		TEST_ASSERT( v3d.y == 20.0 )
+		TEST_ASSERT( v3d.z == 50.0 )
+
+		of::ofVec3<int> v3i(10,40, 50);
+		TEST_ASSERT( v3i.x == 10 )
+		TEST_ASSERT( v3i.y == 40 )
+		TEST_ASSERT( v3i.z == 50 )
 	}
 	
 	void test_instance_creation_4d() {
+			
+		of::ofVec4d v4d( 20.0, 20.0, 50.0, 30.0 );
+		TEST_ASSERT( v4d.x == 20.0 )
+		TEST_ASSERT( v4d.y == 20.0 )
+		TEST_ASSERT( v4d.z == 50.0 )
+		TEST_ASSERT( v4d.w == 30.0 )
 		
-		of::ofVec4f vec4f( 10.0f, 10.0f, 50.0f, 80.0f );
-		of::ofVec4d vec4d( 20.0, 20.0, 50.0, 30.0 );
-		
-		TEST_ASSERT( vec4f.x == 10.0f )
-		TEST_ASSERT( vec4f.y == 10.0f )
-		TEST_ASSERT( vec4f.z == 50.0f )
-		TEST_ASSERT( vec4f.w == 80.0f )
-		
-		TEST_ASSERT( vec4d.x == 20.0 )
-		TEST_ASSERT( vec4d.y == 20.0 )
-		TEST_ASSERT( vec4d.z == 50.0 )
-		TEST_ASSERT( vec4d.w == 30.0 )
+		of::ofVec4<int> v4i(10,40, 50, 22);
+		TEST_ASSERT( v4i.x == 10 )
+		TEST_ASSERT( v4i.y == 40 )
+		TEST_ASSERT( v4i.z == 50 )
+		TEST_ASSERT( v4i.w == 22 )
 	}
 	
+	void test_array_access() {
+		of::ofVec2<float> v2f( 23, 42 );
+		TEST_ASSERT( v2f[ 0 ] == 23 )
+        TEST_ASSERT( v2f[ 1 ] == 42 )
+		
+		of::ofVec4d v4d( 20.0, 20.0, 50.0, 30.0 );
+		TEST_ASSERT( v4d[ 0 ] == 20.0f )
+		TEST_ASSERT( v4d[ 1 ] == 20.0f )
+		TEST_ASSERT( v4d[ 2 ] == 50.0f )
+		TEST_ASSERT( v4d[ 3 ] == 30.0f )
+	}
+
+	void test_equality() {
+		of::ofVecT<int> v1i( 50 );
+        TEST_ASSERT( v1i == of::ofVecT<int>( 50 ) ) 
+		
+        of::ofVec2f v2f( 12.0, 10.0 );
+		of::ofVec2f v2f2( 12.0f, 10.0f );
+		TEST_ASSERT( v2f.x == 12.0f )
+		TEST_ASSERT( v2f.y == 10.0f )
+        TEST_ASSERT( v2f == v2f2 )
+		
+		of::ofVec2<int> v2i( 12, 10 );
+		TEST_ASSERT( v2f == v2i )
+		
+		v2i.set(40, 20);
+		TEST_ASSERT( v2f != v2i )
+    }
+    
+    void test_assignments() {
+        of::ofVec2f v2f( 2, 2 );
+        of::ofVec3f v3f( v2f );
+		TEST_ASSERT( v3f.x == 2.0f )
+		TEST_ASSERT( v3f.y == 2.0f )
+
+		// probably this will loose data. double -> float
+        of::ofVec2d v2d( 3, 3 );
+        of::ofVec3f v3ff( v2d );
+		TEST_ASSERT( v3ff.x == 3.0 )
+		TEST_ASSERT( v3ff.y == 3.0 )
+    }
 };
