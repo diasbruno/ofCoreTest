c6574c3b4d44ea964815219edc8088f5248dc1ee
Bruno Dias <contact@diasbruno.com.br>
2013-02-02 21:48:42 -0300

    moved test methods to ofCoreTest.h.

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,22 @@
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
 
-#include "ofVecT_test.h"
-#include "ofVecT_Operations_test.h"
+#include "ofCoreTests.h"
 
-// 0 output as console 
-// 1 output as html
-#define CPPTEST_OUTPUT 0 
-
-static Test::Suite ts_types;
-
-/*! */
-void run_tests_for_ofVecT() {
-    ts_types.add( auto_ptr<Test::Suite>( new ofVecT_Suite ) );
-    ts_types.add( auto_ptr<Test::Suite>( new ofVecT_Operations_Suite ) );
+void bootApp() {
+	ofAppGlutWindow win;
+	ofSetupOpenGL( &win , 600, 400, OF_WINDOW);
+	ofRunApp( new testApp );
 }
 
-void run_tests() {
-	// comment tests you don't want to run.
-	run_tests_for_ofVecT();
-    
-#if CPPTEST_OUTPUT == 1
-    Test::HtmlOutput output;
-	output.generate( std::cout, true, "HTML Tests" );
-#else
-    Test::TextOutput output( Test::TextOutput::Verbose );
-#endif
-	
-    ts_types.run( output );
-}
-
-/*! */
+/*! 
+ 
+ All tests are placed in ofCore/ofCoreTests.h.
+ 
+ */
 int main() {
+//	bootApp();
+	
 	run_tests();
-    return 0;
+	return 0;
 }
