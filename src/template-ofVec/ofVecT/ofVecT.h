522b3b538391321102b40baed8ba28cc5b7b956b
Bruno Dias <contact@diasbruno.com.br>
2013-02-05 00:23:37 -0300

    ofVecT only needs getPtr() and array accessor.

--- src/template-ofVec/ofVecT/ofVecT.h
+++ src/template-ofVec/ofVecT/ofVecT.h
@@ -7,55 +7,39 @@
 
 namespace of {
 	
-	template<typename T>
-	class ofVecT {
-	public:
-		T x;
-		// T* x[ DIM ] ?
-		
-		static const int DIM = 1;
-		
-		ofVecT()
-		: x(0) 
-		{}
-		
-		ofVecT( T _x )
-		: x(_x) 
-		{}
-		
-		// Equality -------------------------------------
-		
-		template <typename S>
-		bool operator==( const ofVecT<S>& v ) const {
-			return x == v.x;
-		}
-		
-		template <typename S>
-		bool operator!=( const ofVecT<S>& v ) const {
-			return x != v.x;
-		}
-		
-		bool match( const ofVecT<T>& v, T tolerance ) const {
-			return fabs(x - v.x) < tolerance;
-		}
-		
-		// Accessors ------------------------------------
-		
-		T* getPtr() {
-			return (T*)&x;
-		}
-		const T* getPtr() const {
-			return (const T*)&x;
-		}
-		
-		T& operator[]( int n ){
-			return getPtr()[n];
-		}
-		
-		T operator[]( int n ) const {
-			return getPtr()[n];
-		}
-		
-	};
+template<typename T>
+class ofVecT {
+public:
+	T x;
+	// T* x[ DIM ] ?
+	
+	static const int DIM = 1;
+	
+	ofVecT()
+	: x(0) 
+	{}
+	
+	ofVecT( T _x )
+	: x(_x) 
+	{}
+	
+	// Accessors ------------------------------------
+	
+	T* getPtr() {
+		return (T*)&x;
+	}
+	const T* getPtr() const {
+		return (const T*)&x;
+	}
+	
+	T& operator[]( int n ){
+		return getPtr()[n];
+	}
+	
+	T operator[]( int n ) const {
+		return getPtr()[n];
+	}
+	
+};
 	
 }
